7987fd1b8ccbc321fe0802154e690ac7eb033d1a
Apirak <apirakfakin@gmail.com>
2023-12-06 16:08:16 +0700

    homepage edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import './globals.css'
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="">
-        <h1>Welcome to our App</h1>
-        <h1>please enjoy</h1>
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <div className="text-center">
+        <h1 className='text-4xl font-semibold'>Welcome to Event-Planner App</h1>
+        <h4 className='text-2xl mt-5'>More Productivity, More Free time, More Fun in your Life</h4>
       </div>
+      <button className='bg-slate-600 text-white px-5 py-2 rounded-xl mt-5'>Start Now</button>
+
       
     </main>
   )
