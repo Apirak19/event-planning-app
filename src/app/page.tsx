c90f3643bd8921e58a4052f67a1aa9857db7018f
Apirak <apirakfakin@gmail.com>
2023-12-06 16:09:39 +0700

    homepage margin edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <main className="min-h-screen flex flex-col justify-center items-center">
       <div className="text-center">
         <h1 className='text-4xl font-semibold'>Welcome to Event-Planner App</h1>
-        <h4 className='text-2xl mt-5'>More Productivity, More Free time, More Fun in your Life</h4>
+        <h4 className='text-2xl mt-4'>More Productivity, More Free time, More Fun in your Life</h4>
       </div>
-      <button className='bg-slate-600 text-white px-5 py-2 rounded-xl mt-5'>Start Now</button>
+      <button className='bg-slate-600 text-white px-5 py-2 rounded-xl mt-7'>Start Now</button>
 
       
     </main>
