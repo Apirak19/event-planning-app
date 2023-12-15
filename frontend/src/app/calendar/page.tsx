4cf50390a572b4d863de3c12a5119f4ca95efa3a
Apirak <apirakfakin@gmail.com>
2023-12-15 13:47:33 +0700

    timegrid install

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -1,30 +1,30 @@
-"use client"
+"use client";
 
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 export default class DemoApp extends React.Component {
   render() {
-     return (
-       <main className='p-2'>
-      <FullCalendar
-  plugins={[ dayGridPlugin ]}
-  dateClick={this.handleDateClick}
-  initialView="dayGridMonth"
-  weekends={false}
-  
-  events={[
-    { title: 'event 1', date: '2019-04-01' },
-    { title: 'event 2', date: '2019-04-02' }
-  ]}
-/>
+    return (
+      <main className="p-24">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          dateClick={this.handleDateClick}
+          initialView="dayGridMonth"
+          weekends={false}
+          events={[
+            { title: "event 1", date: "2019-04-01" },
+            { title: "event 2", date: "2019-04-02" },
+          ]}
+        />
       </main>
-      
-    )
-   }
-   handleDateClick = (arg: any) => { // bind with an arrow function
-      alert(arg.dateStr)
-    }
+    );
+  }
+  handleDateClick = (arg: any) => {
+    // bind with an arrow function
+    alert(arg.dateStr);
+  };
 }
