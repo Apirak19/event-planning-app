217512cb92ebec2f59092ef713b50cc3cc3fe00f
Apirak <apirakfakin@gmail.com>
2023-12-15 15:57:20 +0700

    calendar attr added

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -6,17 +6,29 @@ import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 import timeGridPlugin from "@fullcalendar/timegrid";
 
+interface Event {
+  title: string;
+  start: Date | string;
+  allDay: Boolean;
+  id: number;
+}
+
 export default class DemoApp extends React.Component {
   render() {
     return (
       <main className="p-24">
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-          dateClick={this.handleDateClick}
+          // dateClick={this.handleDateClick}
           initialView="dayGridMonth"
-          weekends={false}
+          weekends={true}
+          nowIndicator={true}
+          selectable={true}
+          selectMirror={true}
+          editable={true}
+          droppable={true}
           events={[
-            { title: "event 1", date: "2019-04-01" },
+            { title: "event 1", date: "2023-12-01" },
             { title: "event 2", date: "2019-04-02" },
           ]}
         />
