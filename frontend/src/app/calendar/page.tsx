d1746e32d6506ebf37cfa13d5175bea4c5ca9032
Apirak Fakin <apirakfakin@gmail.com>
2023-12-20 06:56:19 +0700

    event edited

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -43,6 +43,7 @@ export default function Calendar() {
           events={[
             { title: "event 1", date: "2023-12-01" },
             { title: "event 2", date: "2019-04-02" },
+            { title: "event 2", date: "2019-04-02" },
           ]}
           />
       </main>
